f8532c9b48e028b1d8fbaf8c6ce21e441d5cf66d
Kevin J. Sung <kevjsung@umich.edu>
2026-02-21 08:41:58 -0500

    whitelist renamed to allowlist in bindgen

--- bin/generate.sh
+++ bin/generate.sh
@@ -7,7 +7,7 @@ set -eux
 #
 # * By default, `char` is converted to `::std::os::raw::c_char`. However, to
 #   preserve no_std, it is truncated to `c_char` and gets taken from `libc`.
-bindgen --whitelist-function='^.*_$' --use-core bin/wrapper.h \
+bindgen --allowlist-function='^.*_$' --use-core bin/wrapper.h \
   | sed -e 's/::std::os::raw:://g' \
   | sed -e '/__darwin_size_t/d' \
   > src/lapack.rs
